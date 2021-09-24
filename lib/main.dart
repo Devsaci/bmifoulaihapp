80392ce847fff9ffff04384b3d6e08a275df3620
saci zakaria <sacideveloppeur@gmail.com>
2021-09-24 10:16:58 +0200

    home: const Result()

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bmifoulaihapp/home.dart';
+import 'package:bmifoulaihapp/result.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: const MyHomePage(),
+      home: const Result(),
     );
   }
 }
