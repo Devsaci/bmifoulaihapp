8274d3ae42208da53b304b47b6f507d9f5b7ac6f
saci zakaria <sacideveloppeur@gmail.com>
2021-09-24 09:34:20 +0200

    Result Construct

--- lib/result.dart
+++ lib/result.dart
@@ -5,7 +5,7 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  const ({Key? key}) : super(key: key);
+  const Result({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
