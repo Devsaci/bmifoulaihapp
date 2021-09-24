a165dd8289044b2c90a87bfc02935ba55634df5e
saci zakaria <sacideveloppeur@gmail.com>
2021-09-24 09:50:38 +0200

    Center()

--- lib/result.dart
+++ lib/result.dart
@@ -1,7 +1,3 @@
-
-
-
-
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
@@ -10,10 +6,13 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Result", style: TextStyle(fontSize: 25),),
+      appBar: AppBar(
+        title: const Text(
+          "Result",
+          style: TextStyle(fontSize: 25),
         ),
-        body: SafeArea(child: child),
-       
+      ),
+      body: SafeArea(child: Center()),
     );
   }
 }
